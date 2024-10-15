296138ba9c7f4464cbc440b1113f6730b8717d00
Jesse <frejes32@gmail.com>
2024-10-14 20:27:35 -0500

    Modified Song model

--- Models/Song.cs
+++ Models/Song.cs
@@ -5,8 +5,11 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public int ArtistId { get; set; }
+        public Artist Artist { get; set; }
         public int AlbumId { get; set; }
+        public Album Album { get; set; }
         public int GenreId { get; set; }
+        public Genre Genre { get; set; }
         public int Length { get; set; }
     }
 }
