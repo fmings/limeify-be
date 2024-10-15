1d5706834c899cb9ae34556a102a42f3cbc1d355
fmings <mingsfelicia@gmail.com>
2024-10-14 20:59:59 -0500

    added image url for album data

--- Models/Album.cs
+++ Models/Album.cs
@@ -3,6 +3,8 @@
     public class Album
     {
         public int Id { get; set; }
+        public string Name { get; set; }
+        public int Year { get; set; }
         public int ArtistId { get; set; }
         public int GenreId { get; set; }
         public string Image {  get; set; }
