b68b04d750b9282c7a15e2458cdd5f8eabaaca08
zach colburn <colburn.zach7@gmail.com>
2024-10-17 19:23:53 -0500

    Get all Songs and Get song by Id

--- API/SongAPI.cs
+++ API/SongAPI.cs
@@ -1,10 +1,37 @@
-﻿namespace Limeify.API
+﻿using Limeify.Models;
+using limeify_be;
+using Microsoft.EntityFrameworkCore;
+
+namespace Limeify.API
 {
     public class SongAPI
     {
         public static void Map(WebApplication app)
         {
+            // GET All Songs
+            app.MapGet("/songs", (LimeifyDbContext db) =>
+            {
+                return db.Songs
+                    .Include(s => s.Artist)
+                    .Include(s => s.Album)
+                    .Include(s => s.Genre)
+                    .ToListAsync();
+            });
 
+            //GET Songs By Id
+            app.MapGet("/songs/{id}", (LimeifyDbContext db, int id) =>
+            {
+                var song = db.Songs
+                    .Include(s => s.Artist)
+                    .Include(s => s.Album)
+                    .Include(s => s.Genre)
+                    .FirstOrDefault(s => s.Id == id);
+                if (song == null)
+                {
+                    return Results.NotFound();
+                }
+                return Results.Ok(song);
+            });
         }
     }
 }
