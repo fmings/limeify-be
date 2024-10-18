8beb234f172dfeb87373ad858f08d121f0ac8750
Jesse <frejes32@gmail.com>
2024-10-17 20:25:40 -0500

    working changes

--- API/ArtistAPI.cs
+++ API/ArtistAPI.cs
@@ -1,10 +1,16 @@
-﻿namespace Limeify.API
+﻿using limeify_be;
+
+namespace Limeify.API
 {
     public class ArtistAPI
     {
         public static void Map(WebApplication app)
         {
-
+            // get a single artist
+            app.MapGet("/api/artists/{id}", (LimeifyDbContext db, int id) =>
+            {
+                return db.Artists.SingleOrDefault(artist => artist.Id == id);
+            });
         }
     }
 }
--- API/SongAPI.cs
+++ API/SongAPI.cs
@@ -1,10 +1,24 @@
-﻿namespace Limeify.API
+﻿using limeify_be;
+
+namespace Limeify.API
 {
     public class SongAPI
     {
         public static void Map(WebApplication app)
         {
+            // add song to playlist
+            app.MapGet("/api/songs/{songId}/add-to-playlist/{playlistId}", (LimeifyDbContext db, int songId, int playlistId) =>
+            {
+                var song = db.Songs.Find(songId);
+                var podcast = db.Playlists.Find(playlistId);
 
+                if (playlistId == null || song == null)
+                {
+                    return Results.NotFound("Playlist or song not found.");
+
+                    // check if the playlist already contains the song
+                }
+            });
         }
     }
 }
