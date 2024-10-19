c658e6bfebf8b507ced0e5c2e503398b3282629c
zach colburn <colburn.zach7@gmail.com>
2024-10-19 09:09:19 -0500

    Get all artist is working

--- API/ArtistAPI.cs
+++ API/ArtistAPI.cs
@@ -1,10 +1,15 @@
-﻿namespace Limeify.API
+﻿using limeify_be;
+
+namespace Limeify.API
 {
     public class ArtistAPI
     {
         public static void Map(WebApplication app)
         {
-
+            app.MapGet("/api/artist", (LimeifyDbContext db) =>
+            {
+                return db.Artists.ToList();
+            });
         }
     }
 }
